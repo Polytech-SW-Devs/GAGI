b00347816f1c66d1db6e41be35bfc3424bfe08ac
이영준 <youngjunlee1120@gmail.com>
2025-09-24 16:17:04 +0900

    썸네일 db추가 item_images db 변경

--- src/main/java/com/exam/gagi/model/ItemImage.java
+++ src/main/java/com/exam/gagi/model/ItemImage.java
@@ -1,11 +1,29 @@
 package com.exam.gagi.model;
 
+import java.util.List;
+
 public class ItemImage extends BaseEntity {
 
 	private int id; // 이미지 ID
 	private int itemId; // 상품 ID (FK)
 	private String imageUrl; // 이미지 경로
 	private int sortOrder; // 정렬 순서
+	// 원본 파일명
+	private String filename;
+	// 서버 저장용 추가 파일명
+	private String uuid;
+
+	// 썸네일 리스트 (1:N)
+	private List<ItemThumbnails> thumbnails;
+
+	// getter / setter
+	public List<ItemThumbnails> getThumbnails() {
+		return thumbnails;
+	}
+
+	public void setThumbnails(List<ItemThumbnails> thumbnails) {
+		this.thumbnails = thumbnails;
+	}
 
 	public int getId() {
 		return id;
@@ -37,6 +55,22 @@ public class ItemImage extends BaseEntity {
 
 	public void setSortOrder(int sortOrder) {
 		this.sortOrder = sortOrder;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
 	}
 
 }
